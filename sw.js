ee011e7bc6374c1c28e41709d1ef5136ea184a06
Nono-82 <a.coquerel@laposte.net>
2026-02-17 11:48:52 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'coquerel-v35-final';
+const CACHE_NAME = 'coquerel-v36-final';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -9,7 +9,13 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))));
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
